9374b83c65a6d9f1f285e43c6ae2fcecf505c4c5
Unknown <dani_mrx93@hotmail.com>
2021-11-11 14:58:18 -0300

    Re-order folder and spells

--- Objects/Spells/RangeSpell.gd
+++ Objects/Spells/RangeSpell.gd
@@ -3,7 +3,12 @@ extends "res://Objects/Spells/Spell.gd"
 export var speed : float = 600.0
 var velocity : Vector2
 
+func cast(_spell_owner : Node2D, spell_position : Vector2, spell_direction : Vector2):
+	.cast(_spell_owner, spell_position, spell_direction)
+	velocity = direction * speed
+	rotation = atan2(direction.y, direction.x)
+	pass
+
 func _physics_process(delta):
-	velocity.x = speed
-	velocity = move_and_slide(velocity)
+	translate(velocity * delta)
 	pass
--- Objects/Wizards/SpellCast.gd
+++ Objects/Wizards/SpellCast.gd
@@ -2,22 +2,28 @@ extends Node
 
 var ice_spell  = preload("res://Objects/Spells/IceStake/IceStake.tscn")
 var rock_spell  = preload("res://Objects/Spells/Rock/TheRock.tscn")
-export (NodePath) var cast_position_node
-export (NodePath) var spell_particles_node
+export (NodePath) var cast_position_path
+export (NodePath) var spell_particles_path
+export (NodePath) var cast_direction_path
 
-onready var cast_position : Position2D = get_node(cast_position_node)
-onready var spell_particles : Particles2D = get_node(spell_particles_node)
+onready var cast_position : Position2D = get_node(cast_position_path)
+onready var spell_particles : Particles2D = get_node(spell_particles_path)
+onready var cast_direction_node : Position2D = get_node(cast_direction_path)
 onready var spell_owner : Wizard = get_parent()
 onready var first_skill : String = "first_skill_" + str(spell_owner.player_id)
 onready var second_skill : String = "second_skill_" + str(spell_owner.player_id)
 
 var spell_list : Array = [ice_spell, rock_spell]
-var current_spell = null
+var current_spell : Node2D = null
+var cast_direction : Vector2 = Vector2.RIGHT
 
 export var is_casting : bool = false
 
+signal on_casting_spell(is_casting)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	cast_direction_node.visible = is_casting
 	pass # Replace with function body.
 
 func _physics_process(delta):
@@ -25,25 +31,39 @@ func _physics_process(delta):
 		_cast_skill(0)
 	if Input.is_action_just_pressed(second_skill):
 		_cast_skill(1)
+	check_skill_direction()
 	check_skill_release()
+	pass
+
+func check_skill_direction():
+	if !is_casting: return
+	cast_direction = Vector2(spell_owner.get_horizontal_input(), spell_owner.get_vertical_input())
+	cast_direction = cast_direction.normalized()
+	if cast_direction == Vector2.ZERO:
+		cast_direction = spell_owner.get_visual_direction()
+	cast_direction_node.rotation = atan2(cast_direction.y, cast_direction.x)
 	pass
 
 func check_skill_release():
 	if !is_casting: return
 	if Input.is_action_just_released(first_skill) or Input.is_action_just_released(second_skill):
 		is_casting = false
+		emit_signal("on_casting_spell", is_casting)
 		get_tree().root.add_child(current_spell)
-		current_spell.call_deferred("cast", spell_owner, cast_position.global_position, get_cast_direction())
+		current_spell.call_deferred("cast", spell_owner, cast_position.global_position, cast_direction)
 		current_spell = null
-		spell_particles.emitting = false
+		spell_particles.emitting = is_casting
+		cast_direction_node.visible = is_casting
 	pass
 
 func _cast_skill(idx : int):
 	if is_casting: return
 	is_casting = true
+	emit_signal("on_casting_spell", is_casting)
 	current_spell = spell_list[idx].instance()
-	spell_particles.emitting = true
+	spell_particles.emitting = is_casting
+	cast_direction_node.visible = is_casting
 	pass
 
 func get_cast_direction():
-	return Vector2(0,1)
+	return cast_direction
