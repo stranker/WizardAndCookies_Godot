b07f6354d14cde3bef8d98f96445ada5bcf30d4e
Gaston <gsvillalba12@gmail.com>
2021-11-13 00:04:46 +0100

    Add Character Selection screen (not hooked)

---
+++ Objects/UI/CharacterSelection/PortraitButton.gd
@@ -0,0 +1,23 @@
+extends Control
+
+onready var name_label 	= $Name
+onready var portrait 	= $Portrait
+onready var highlight 	= $Highlight
+
+export var name_text : String
+export var portrait_URL : Texture
+export var player_color : Color
+
+func _ready() -> void:
+	name_label.text = name_text
+	portrait.texture = portrait_URL
+	highlight.modulate = player_color
+	pass
+
+func _on_TextureButton_focus_entered() -> void:
+	highlight.visible = true
+	pass
+
+func _on_TextureButton_focus_exited() -> void:
+	highlight.visible = false
+	pass
