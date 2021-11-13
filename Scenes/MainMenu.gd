cdc9bf4f11dac3e10c559c35a4cd263c19ed7c7a
Gaston <gsvillalba12@gmail.com>
2021-11-13 01:04:18 +0100

    Zarlanga

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -4,9 +4,12 @@ onready var scene_manager   = get_node("/root/SceneManager")
 onready var main_menu_panel = $MainMenuPanel
 onready var settings_panel 	= $SettingsPanel
 onready var animation_player = $AnimationPlayer
+onready var play_button = $MainMenuPanel/VBoxContainer/PlayBtn
+onready var back_button = $SettingsPanel/VBoxContainer/HBoxContainer/SettingsBackBtn
 
 func _ready() -> void:
 	animation_player.play("MainMenu_Show")
+	play_button.grab_focus()
 	pass
 
 func _on_PlayBtn_pressed() -> void:
@@ -21,10 +24,12 @@ func _on_ExitBtn_pressed() -> void:
 func _on_SettingsBtn_pressed() -> void:
 	main_menu_panel.visible = false
 	settings_panel.visible 	= true
+	back_button.grab_focus()
 	pass
 
 
 func _on_SettingsExitBtn_pressed() -> void:
 	main_menu_panel.visible = true
 	settings_panel.visible 	= false
+	play_button.grab_focus()
 	pass
