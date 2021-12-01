6448a15323f7cc491b495a7427054b159e498337
Unknown <dani_mrx93@hotmail.com>
2021-12-01 19:21:46 -0300

    Add UI Spell to gameplay UI

--- Objects/UI/Gameplay/PlayerInfo.gd
+++ Objects/UI/Gameplay/PlayerInfo.gd
@@ -3,22 +3,17 @@ extends Control
 onready var hp_bar   : TextureProgress = $ProgressBars/HpBar
 onready var fly_bar : TextureProgress = $ProgressBars/FlyBar
 onready var character_portrait : TextureRect = $PotraitHolder/Background/Mask/Portrait
-onready var Spell1 : Control = $SpellsHolder/Spell1
-onready var Spell2 : Control = $SpellsHolder/Spell2
-onready var Spell3 : Control = $SpellsHolder/Spell3
+onready var spells : Array = $SpellsHolder/SpellsContainer.get_children()
 
 export var hp : int
 export var mana : int
 export var character_portrait_texture : Texture
-export var spell1_portrait_texture : Texture
-export var spell2_portrait_texture : Texture
-export var spell3_portrait_texture : Texture
+export (Array, Texture) var spells_texture
 
 func _ready() -> void:
 	hp_bar.value = hp
 	fly_bar.value = mana
 	character_portrait.texture = character_portrait_texture
-	$SpellsHolder/HBoxContainer/Spell1/SpellPortrait.texture = spell1_portrait_texture
-	$SpellsHolder/HBoxContainer/Spell2/SpellPortrait.texture = spell2_portrait_texture
-	$SpellsHolder/HBoxContainer/Spell3/SpellPortrait.texture = spell3_portrait_texture
+	for i in range(spells.size()):
+		spells[i].set_spell_texture(spells_texture[i])
 	pass
